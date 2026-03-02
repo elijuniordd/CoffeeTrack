1e44e3cbf93b61db99a0bc9389d8b21940365908
Eli Dias <elidias@frwk.com.br>
2026-03-02 15:28:04 -0300

    feat: implement buyerPortal functionality with LWC and integrate getLotes wire service

--- force-app/main/default/lwc/buyerPortal/buyerPortal.js
+++ force-app/main/default/lwc/buyerPortal/buyerPortal.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import getLotes from '@salesforce/apex/CoffeeTrackController.getLotesDisponiveis';
 
-export default class BuyerPortal extends LightningElement {}
+export default class BuyerPortal extends LightningElement {
+    @track notaMinima = 0;
+    @track lotes;
+
+    @wire(getLotes, { notaMinima: '$notaMinima' })
+    wiredLotes({ error, data }) {
+        if (data) this.lotes = data;
+        else if (error) console.error(error);
+    }
+
+    handleNotaChange(event) {
+        this.notaMinima = event.target.value;
+    }
+
+    handleCotacao() {
+        // Aqui entra o Mock da Integração
+        const taxaMock = 5.05; 
+        alert('Cotação atual simulada: R$ ' + taxaMock + ' por Dólar (API Mock)');
+    }
+}
